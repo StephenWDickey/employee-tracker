a1c2f5221267c5e33bf1fbfa62eb09aa6ec2f5dc
stevey d <stphndcky22@gmail.com>
2022-04-23 17:09:58 -0700

    added data to seeds.sql file to populate tables

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+-- we made a seeds.sql file so we can autopopulate our tables!
+-- im gonna get info from the mockup
+INSERT INTO departments (name)
+VALUES
+    ('Sales'),
+    ('Finance'),
+    ('Engineering'),
+    ('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Salesperson', '80000', 1),
+    ('Sales Lead', '100000', 1),
+    ('Accountant', '125000', 2),
+    ('Account Manager', '160000', 2),
+    ('Software Engineer', '120000', 3),
+    ('Lead Engineer', '150000', 3),
+    ('Lawyer', '190000', 4),
+    ('Legal Team Lead', '250000', 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES;
+    ('Mike', 'Chan', 1, 2),
+    ('John', 'Doe', 2, null),
+    ('Malia', 'Brown', 3, 4),
+    ('Kunal', 'Singh', 4, null),
+    ('Kevin', 'Tupik', 5, 6),
+    ('Ashley', 'Rodriguez', 6, null),
+    ('Tom', 'Allen', 7, 8),
+    ('Sarah', 'Lourd', 8, null);
